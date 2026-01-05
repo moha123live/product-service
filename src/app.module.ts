e6496d60e92b561b4e8121a9f1be230e5f36e9ff
wedo-websites <admin@wedo-websites.com>
2026-01-05 22:22:19 +0530

    db added and product module has added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './config/database.config';
+import { join } from 'path';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(__dirname, '../../.env'),
+    }),
+    TypeOrmModule.forRoot(databaseConfig),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
